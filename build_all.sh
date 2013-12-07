aaf35f4b347223f19e32922a4be50c2eb315923b
blalor <blalor@bravo5.org>
2013-12-07 21:20:36 +0000

    Using own centos base image, now
    Turns out with centos 6.5, /sbin/init is no longer friendly to Docker. :-(

--- build_all.sh
+++ build_all.sh
@@ -4,13 +4,12 @@ set -e
 
 cd $( dirname $0 )
 
-docker build -t blalor/centos:init-friendly centos-init-friendly
-docker build -t blalor/centos:java centos-java
+docker build -t blalor/java:1.7.0 centos-java
 
-docker build -t blalor/kibana kibana
+docker build -t blalor/kibana:3.0.0milestone4 kibana
 
-docker build -t blalor/elasticsearch:base elasticsearch-base
-docker build -t blalor/logstash:base logstash-base
-
+docker build -t blalor/elasticsearch:0.90.7 elasticsearch-base
 docker build -t blalor/elasticsearch:private elasticsearch
+
+docker build -t blalor/logstash:1.2.2 logstash-base
 docker build -t blalor/logstash:private logstash
